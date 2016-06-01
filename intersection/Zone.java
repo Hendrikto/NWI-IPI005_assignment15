3ae40b2b48c60df30c06c29ba822f434e3c53ede
Hendrikto <hendrik.to@gmail.com>
2016-06-01 15:31:29 +0200

    Add a class to represent zones on an intersection.

---
+++ intersection/Zone.java
@@ -0,0 +1,34 @@
+package intersection;
+
+/**
+ * A zone on an intersection.
+ *
+ * @author Hendrik Werner // s4549775
+ */
+public class Zone {
+
+    private boolean blocked;
+
+    /**
+     * @return whether blocking was successful
+     */
+    public boolean block() {
+        if (blocked) {
+            return false;
+        }
+        blocked = true;
+        return true;
+    }
+
+    /**
+     * @return whether releasing was successful
+     */
+    public boolean release() {
+        if (!blocked) {
+            return false;
+        }
+        blocked = false;
+        return true;
+    }
+
+}
