1aba1f7cf76042ff7a11bdc911632ab8de18d4f1
Hendrik Werner <hendrik.to@gmail.com>
2016-06-04 15:25:02 +0200

    Create the four zones.

--- assignment15/Assignment15.java
+++ assignment15/Assignment15.java
@@ -1,5 +1,7 @@
 package assignment15;
 
+import intersection.Zone;
+
 /**
  *
  * @author Hendrik Werner // s4549775
@@ -11,7 +13,10 @@ public class Assignment15 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Zone z0 = new Zone(0);
+        Zone z1 = new Zone(1);
+        Zone z2 = new Zone(2);
+        Zone z3 = new Zone(3);
     }
 
 }
