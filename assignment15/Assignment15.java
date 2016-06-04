feacab484a7ea9a2b8832703e8aa7a43a47445eb
Hendrik Werner <hendrik.to@gmail.com>
2016-06-05 01:40:48 +0200

    Increase readability.

--- assignment15/Assignment15.java
+++ assignment15/Assignment15.java
@@ -19,15 +19,10 @@ public class Assignment15 {
         Zone z1 = new Zone(1);
         Zone z2 = new Zone(2);
         Zone z3 = new Zone(3);
-
-        Car c0 = new Car(0, z0, z1, z2);
-        Car c1 = new Car(1, z1, z2, z3);
-        Car c2 = new Car(2, z2, z3, z0);
-        Car c3 = new Car(3, z3, z0, z1);
-        new Thread(c0).start();
-        new Thread(c1).start();
-        new Thread(c2).start();
-        new Thread(c3).start();
+        new Thread(new Car(0, z0, z1, z2)).start();
+        new Thread(new Car(1, z1, z2, z3)).start();
+        new Thread(new Car(2, z2, z3, z0)).start();
+        new Thread(new Car(3, z3, z0, z1)).start();
     }
 
 }
