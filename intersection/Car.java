a5db8573411750adebd931427730e3d00f293e6c
Hendrik Werner <hendrik.to@gmail.com>
2016-06-04 15:08:41 +0200

    Add some attributes.

--- intersection/Car.java
+++ intersection/Car.java
@@ -7,4 +7,19 @@ package intersection;
  */
 public class Car {
 
+    private final int ID;
+    private final Zone[] zones;
+
+    /**
+     * @param id the id of the car
+     * @param zones the zones this car blocks when crossing the intersection.
+     * These must be ordered: When this car turns right it blocks only the first
+     * zone when it goes straight of blocks the first two zones and when turning
+     * left it block the first three.
+     */
+    public Car(int id, Zone... zones) {
+        ID = id;
+        this.zones = zones;
+    }
+
 }
