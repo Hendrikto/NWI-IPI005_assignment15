6b1fdb81b1cb5142da4f8f5e3f66825cac9c220d
jhaasdijk <jasperhaasdijk@hotmail.com>
2016-06-05 12:35:07 +0200

    Update authors

--- intersection/Direction.java
+++ intersection/Direction.java
@@ -6,6 +6,7 @@ import java.util.Random;
  * Directions a car can take at an intersection.
  *
  * @author Hendrik Werner // s4549775
+ * @author Jasper Haasdijk // s4449754
  */
 public enum Direction {
     Right, Straight, Left;
