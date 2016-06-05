7b0cd7d1106c26fb292e5433c825baf5492666cc
jhaasdijk <jasperhaasdijk@hotmail.com>
2016-06-05 12:35:47 +0200

    Update authors

--- intersection/Car.java
+++ intersection/Car.java
@@ -9,6 +9,7 @@ import java.util.logging.Logger;
  * A car that can ride straight, right or left.
  *
  * @author Hendrik Werner // s4549775
+ * @author Jasper Haasdijk // s4449754
  */
 public class Car implements Runnable {
 
